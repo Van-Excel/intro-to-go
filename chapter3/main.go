ff2ce5e729559e3f32b5b97079043ca0c3a537f3
Van-Excel <vanexcel.investments@gmail.com>
2025-10-21 20:23:41 +0000

    updated chapter3

--- chapter3/main.go
+++ chapter3/main.go
@@ -12,6 +12,14 @@ func main() {
 	fmt.Printf("The fullname is %s %s\n", fName, lName)
 	fmt.Println("The fullname concatenated is:", fName+" "+lName)
 
+	//declare variable
+	var school string
+	school = "alpha beta"
+	fmt.Println("name of school:", school)
+	school = "martin de porres"
+	fmt.Println("name of new school is:", school)
+	fmt.Println("is your school alpha beta?", school == "alpha beta")
+
 	// variables are mappings of human readable names to memory addresses
 	// you are essentially telling the compiler to allocate some space
 	// and map the address of that memory block to a human readable name
